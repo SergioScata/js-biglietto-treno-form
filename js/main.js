91c6af794bcc9ba8470f2e819a43148244bbfae1
SergioScata <sergiopio395@gmail.com>
2023-11-14 15:07:25 +0100

    Updated js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
 //Richiediamo all'utente i dati di cui abbiamo bisogno
-let travellength= parseFloat(prompt("Inserisci quanti km vuoi percorrere"));
-let age= parseInt(prompt("Inserisci la tua età"));
-let ticketprice= (travellength * 0.21);
-let finalprice = ticketprice.toFixed(2);
-
-
-//Sezione degli if
-if (age>=65) {
-   ticketprice= (ticketprice * 0.6);
-}
-else if (age<18) {
-    ticketprice= (ticketprice * 0.8);
-}
-else {
-    ticketprice= ticketprice;
-}
 
 //riportiamo il messaggio sia nella console che in Html
-console.log ("Il prezzo del tuo biglietto è:" + finalprice);
 
-document.getElementById("utent-price").innerHTML="Il prezzo del tuo biglietto è €" + finalprice;
+
+function userdonedatacompletion(event) {
+    event.preventDefault();
+
+    let namesurname = document.getElementById("namesurname").value;
+    let travellength = document.getElementById("travellength").value;
+    let age = document.getElementById("age").value;
+
+    travellength= parseFloat(travellength);
+    age = parseInt(age);
+
+    let ticketprice= (travellength * 0.21);
+    let finalprice = ticketprice.toFixed(2);
+
+
+    if (age>=65) {
+        ticketprice= (ticketprice * 0.6);
+     }
+     else if (age<18) {
+         ticketprice= (ticketprice * 0.8);
+     }
+     else {
+         ticketprice= ticketprice;
+     }
+
+
+
+    }
+
+
+console.log ("Il prezzo del tuo biglietto è:" + finalprice);
